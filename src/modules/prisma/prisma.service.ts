afc41c8fa6e56d56fe9577cdaacdf7a3d2016450
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-07-17 21:46:41 +0330

    feat: add modular stracture

---
+++ src/modules/prisma/prisma.service.ts
@@ -0,0 +1,28 @@
+import {
+  Injectable,
+  Logger,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleDestroy, OnModuleInit
+{
+  private readonly logger: Logger = new Logger(PrismaService.name);
+  constructor() {
+    super();
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Database connected successfully.');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    this.logger.log('Discounted from database.');
+  }
+}
