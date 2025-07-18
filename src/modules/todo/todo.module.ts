058e70f93bf329e501c10eb1929154f3132445e4
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-07-18 16:02:32 +0330

    feat: add user and todo repositories

--- src/modules/todo/todo.module.ts
+++ src/modules/todo/todo.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { TodoController } from './todo.controller';
+import { PrismaService } from '../prisma/prisma.service';
+import { TodoRepository } from './todo.repository';
 
 @Module({
   controllers: [TodoController],
-  providers: [TodoService],
+  providers: [TodoService, PrismaService, TodoRepository],
 })
-export class TodoModule {}
+export class TodoModule { }
